a4cb1b0ad8edc0ee987a4053b1931428c3383594
yamadajc <74032620+yamadajc@users.noreply.github.com>
2021-02-23 04:36:49 +0100

    Add files via upload

---
+++ api-sentiment-project/application/helpers/endpoints.py
@@ -0,0 +1,75 @@
+from helpers.mongoConnection import *
+from helpers.checking import *
+from bson import ObjectId
+from helpers.settings import *
+
+def setup(typ):
+    if typ == "masterchef":
+        return masterchef_set
+    elif typ == "character":
+        return character_set
+    elif typ == "twitter":
+        return twitter_set
+
+def insert(obj,typ):
+    settings = setup(typ)
+
+    if not check_params(obj,settings['obligatory']):
+        return {"response":400, "message": f"Bad Request: {settings['obligatory']} are obligatory parameters"}
+
+    q = {settings['core']:obj[settings['core']]}
+
+    if check_exists(q,typ):
+        return {"response":400,"message": f"Bad Request: there is already a {settings['sing']} with that name"}
+
+    res = write_coll(typ,obj)
+    return {"response":200,"message": f"{settings['sing']}  successfully added"}
+    
+
+def list_all(typ):
+    settings = setup(typ)
+
+    res = read_coll(typ,{})
+    
+    return res
+
+def get(obj,typ):
+    settings = setup(typ)
+
+    if not check_params(obj,['id']):
+        return {"response":400,"message":"Bad Request: 'id' is an obligatory parameter"}
+
+    q = {"_id":ObjectId(obj['id'])}
+
+    if not check_exists(q,typ):
+        return {"response":400,"message": f"Bad Request: {settings['sing']} with given id does not exist"}
+    
+    return read_coll(typ,q)
+
+def delete(obj,typ):
+    settings = setup(typ)
+
+    if not check_params(obj,['id']):
+        return {"response":400,"message":"Bad Request: 'id' is an obligatory parameter"}
+
+    q = {"_id":ObjectId(obj['id'])}
+    if not check_exists(q,typ):
+        return {"response":400,"message": f"Bad Request: {settings['sing']} with given id does not exist"}
+
+    delete_coll(typ,q)
+    return {"response":200,"message": f"{settings['sing']}  successfully deleted"}
+
+def update(obj,typ):
+    settings = setup(typ)
+
+    if not check_params(obj,['id'],settings['at_least_one']):
+        return {"response":400,"message": f"Bad Request: 'id' and at least one of {settings['at_least_one']} are obligatory parameters"}
+    
+    q = {"_id":ObjectId(obj['id'])}
+    if not check_exists(q,typ):
+        return {"response":400,"message": f"Bad Request: {settings['sing']} with given id does not exist"}
+    
+    obj.pop("id")
+    update_coll(typ,q,obj)
+    return {"response":200, "message": f"{settings['sing']} successfully updated"}
+
---
+++ api-sentiment-project/application/helpers/settings.py
@@ -0,0 +1,18 @@
+masterchef_set = {
+    "obligatory":["name","phrase"],
+    "at_least_one":["name","phrase","season","chapter"],
+    "sing":"masterchef",
+    "core":"phrase"
+}
+character_set = {
+    "obligatory":["name"],
+    "at_least_one":["name","role"],
+    "sing":"character",
+    "core":"role"
+}
+twitter_set = {
+    "obligatory":["name","tweet"],
+    "at_least_one":["name","tweet"],
+    "sing":"twitter",
+    "core":"tweet"
+}
